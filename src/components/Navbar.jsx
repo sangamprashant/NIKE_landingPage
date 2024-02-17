7f15ab8bbdf06da1aa43bd7678b846dbed655f3c
aryan singh <singharyan5656@gmail.com>
2024-02-17 13:56:40 +0530

    added navbar options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import {headerLogo} from  '../assets/images'
 import { hamburger} from '../assets/icons'
-
+import { navLinks } from '../constants'
 const Navbar = () => {
   return (
  <header className='padding-x py-8 absolute z-10 w-full'>
@@ -13,6 +13,28 @@ const Navbar = () => {
 
             />
         </a>
+        <ul className='flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item)=>(
+          <li key={item.label}>
+            <a 
+            href={item.href}
+            className='fonts-montserrat leading-normal text-lg text-slate-gray'
+            >
+              {item.label}
+            </a>
+          </li>
+          ))}
+        </ul>
+        <div className='"hidden 
+        max-lg:block'>
+          <img 
+          src={hamburger}
+          alt="hamburger"
+          height={25}
+          width={25}
+/>
+
+        </div>
     </nav>
  </header>
   )
