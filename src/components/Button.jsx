e698e4674ac7718cc3a1197f33922a32e8426a9d
Prashant Srivastav <srivastavp891@gmail.com>
2024-02-17 15:05:08 +0530

    Bug:props data and path

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,16 @@
-
-const Button = () => {
+const Button = ({ label, iconURL }) => {
   return (
     <button className="">
-    {label}
+      {label}
       <img
-      src={iconURL}
-      alt="arrow right icon"
-      className="ml-2 rounded-full"
-      w-5 h-5 />
-      </button>
-  )
-}
+        src={iconURL}
+        alt="arrow right icon"
+        className="ml-2 rounded-full"
+        w-5
+        h-5
+      />
+    </button>
+  );
+};
 
-export default Button
+export default Button;
